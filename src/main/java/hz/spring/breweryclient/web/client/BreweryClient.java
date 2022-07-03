1a090c90c6144cfd79c0c1fb32da445e8c29c7e1
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-03 01:12:57 -0400

    add post method

--- src/main/java/hz/spring/breweryclient/web/client/BreweryClient.java
+++ src/main/java/hz/spring/breweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -25,6 +26,11 @@ public class BreweryClient {
     public BeerDTO getBeerById(UUID uuid){
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDTO.class);
     }
+
+    public URI saveNewBeer(BeerDTO beerDTO) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDTO);
+    }
+
 
     public void setApihost(String apihost) {
         this.apihost = apihost;
